2fd9bd05a2793b87948259dd7db6ed9d46525042
WJJ-Company <1780409106@qq.com>
2023-12-08 14:21:24 +0800

    add 文远项目

---
+++ 文远/MBom/查询工艺所需要的零件信息.sql
@@ -0,0 +1,19 @@
+select
+    Part.ITEM_NUMBER PartNumber,
+    Part.NAME PartName,
+    Part.HS_NAME_EN PartEnName,
+    Part.HS_LARGE_CLASS PartCategory,
+    Part.MAJOR_REV PartVersion,
+    Part.GENERATION PartGeneration,
+    isnull(Part.HS_UNIT,N'个') as Unit,
+    State.LABEL_ZC PartState,
+    isnull(State.LABEL,State.NAME) PartEnState,
+    Part.CREATED_ON CreateOn,
+    u.KEYED_NAME CreateBy
+from innovator.PART Part
+left join innovator.LIFE_CYCLE_STATE State on Part.CURRENT_STATE = State.ID
+left join innovator.[USER] U on U.id = Part.CREATED_BY_ID;
+
+select *
+from innovator.LIFE_CYCLE_STATE
+where id = 'EB5B395C3518438098B8EA33B92667F7';
---
+++ 文远/MBom/根据零件，查询所有子阶零件.sql
@@ -0,0 +1,26 @@
+--找到同一个source_id的数据为多个的source_id
+select SOURCE_ID,count(1) from innovator.PART_BOM
+group by SOURCE_ID;
+
+select KEYED_NAME
+from innovator.PART
+where id = 'DA0B2D33098B4DE1BC3C00DCE49395A1';
+
+--查询该id下所有的父子关系
+WITH RecursiveCTE AS (
+    -- 递归的起始点
+    SELECT source_id,RELATED_ID, QUANTITY
+    FROM innovator.PART_BOM
+    WHERE source_id = '2E00DD06AB40408389A66126457EF747' -- 指定一个特定的起始点
+
+    UNION ALL
+
+    -- 递归查询部分
+    SELECT bom.source_id,bom.RELATED_ID, bom.QUANTITY
+    FROM innovator.PART_BOM bom
+    INNER JOIN RecursiveCTE rcte ON bom.source_id = rcte.RELATED_ID
+)
+-- 零件与零件之间的关系只能有一种，去重
+SELECT distinct source_id parentId,RELATED_ID id, quantity FROM RecursiveCTE
+;
+
